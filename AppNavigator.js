def7209ea42bf156d3b2f012302a457777fdd165
NojusIlekis <b00100794@mytudublin.ie>
2024-11-26 15:01:29 +0000

    added navigator titles

--- AppNavigator.js
+++ AppNavigator.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import HomeScreen from "./HomeScreen";
+import HomeScreen from "./HomeScreen"; // Ensure the paths are correct
 import LoginScreen from "./LoginScreen";
 
 const Stack = createStackNavigator();
@@ -10,8 +10,18 @@ const AppNavigator = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={LoginScreen} />
-        <Stack.Screen name="Home" component={HomeScreen} />
+        {/* Define Login Screen */}
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{ title: "Login Page" }}
+        />
+        {/* Define Home Screen */}
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ title: "Dashboard" }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
