df6748c01d710e13a87cf18721874085115ffeb3
NojusIlekis <b00100794@mytudublin.ie>
2024-11-26 20:33:58 +0000

    fix2

--- AppNavigator.js
+++ AppNavigator.js
@@ -1,8 +1,9 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import HomeScreen from "./HomeScreen"; // Ensure the paths are correct
+import HomeScreen from "./HomeScreen"; 
 import LoginScreen from "./LoginScreen";
+import ProfileScreen from "./ProfileScreen";
 
 const Stack = createStackNavigator();
 
@@ -10,17 +11,23 @@ const AppNavigator = () => {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
-        {/* Define Login Screen */}
+        {/* Login Screen */}
         <Stack.Screen
           name="Login"
           component={LoginScreen}
           options={{ title: "Login Page" }}
         />
-        {/* Define Home Screen */}
+        {/* Home Screen */}
         <Stack.Screen
           name="Home"
           component={HomeScreen}
           options={{ title: "Dashboard" }}
+        />
+        {/* Profile Screen */}
+        <Stack.Screen
+          name="Profile"
+          component={ProfileScreen}
+          options={{ title: "User Profile" }}
         />
       </Stack.Navigator>
     </NavigationContainer>
